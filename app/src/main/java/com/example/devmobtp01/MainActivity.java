18d7f7384086d44f9d2f645de6882e4bc700b2d1
COUNILLE Alexandra <54265816+FlooneMalubo@users.noreply.github.com>
2021-01-30 13:56:57 +0100

    Finission gabarit en Java

--- app/src/main/java/com/example/devmobtp01/MainActivity.java
+++ app/src/main/java/com/example/devmobtp01/MainActivity.java
@@ -1,12 +1,14 @@
-package com.example.devmobtp01;
+    package com.example.devmobtp01;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.InputType;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,40 +22,70 @@ public class MainActivity extends Activity {
     private EditText phone;
     private Button validate;
 
+    private Button seeLayout;
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.formulaire);
 
-        formulaire = (TextView) findViewById(R.id.formulaire);
+        //setContentView(R.layout.formulaire);
+
+        LinearLayout LL = new LinearLayout(this);
+        LL.setOrientation(LinearLayout.VERTICAL);
+
+        formulaire = new TextView(this);
         formulaire.setText(R.string.welcome);
+        formulaire.setTextSize(20);
 
-        name = (EditText) findViewById(R.id.name);
+        name = new EditText(this);
         name.setHint(R.string.name);
         name.setInputType(InputType.TYPE_CLASS_TEXT);
 
-        firstName = (EditText) findViewById(R.id.firstName);
+        firstName = new EditText(this);
         firstName.setHint(R.string.firstName);
         firstName.setInputType(InputType.TYPE_CLASS_TEXT);
 
-        age = (EditText) findViewById(R.id.age);
+        age = new EditText(this);
         age.setHint(R.string.age);
         age.setInputType(InputType.TYPE_CLASS_NUMBER);
 
-        domaine = (EditText) findViewById(R.id.domain);
+        domaine = new EditText(this);
         domaine.setHint(R.string.domain);
         domaine.setInputType(InputType.TYPE_CLASS_TEXT);
 
-        phone = (EditText) findViewById(R.id.num);
+        phone = new EditText(this);
         phone.setHint(R.string.num);
         phone.setInputType(InputType.TYPE_CLASS_PHONE);
 
-        validate = (Button) findViewById(R.id.ok);
+        validate = new Button(this);
+        validate.setText(R.string.validate);
         validate.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View arg0) {
                 Toast.makeText(getApplicationContext(),R.string.isValidate, Toast.LENGTH_LONG).show();
             }
         });
+
+        seeLayout = new Button(this);
+        seeLayout.setText(R.string.click);
+        seeLayout.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Intent next = new Intent(MainActivity.this, com.example.devmobtp01.SecondActivity.class);
+                startActivity(next);
+            }
+        });
+
+        LL.addView(formulaire);
+        LL.addView(name);
+        LL.addView(firstName);
+        LL.addView(age);
+        LL.addView(domaine);
+        LL.addView(phone);
+        LL.addView(validate);
+        LL.addView(seeLayout);
+
+        setContentView(LL);
+
     }
 
 }
